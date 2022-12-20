f10ce87b5f838cf4e5a1014821f78bf432082718
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 16:41:36 +0100

    Finita seconda query, corsi del dipartimento di neuroscienze

--- JOIN_2.sql
+++ JOIN_2.sql
@@ -1,3 +1,8 @@
 ﻿/*
- * 
+ * Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
  */
+
+select departments.[name], [degrees].[name], [degrees].[level]
+from dbo.departments
+join dbo.[degrees] on [degrees].department_id = departments.id
+where departments.[name] like '%Neuroscienze%' and [degrees].[level] like 'Magistrale';
