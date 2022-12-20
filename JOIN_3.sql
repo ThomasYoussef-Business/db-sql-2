0e97467e17abc2d2b6d9a5dbf99d94707da951c0
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 16:52:37 +0100

    Finita terza query, tutti i corsi di Fulvio Amato

--- JOIN_3.sql
+++ JOIN_3.sql
@@ -1,3 +1,9 @@
 ﻿/*
- * 
+ * Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
  */
+
+ select *
+ from course_teacher
+ join teachers on teachers.id = course_teacher.teacher_id
+ join courses on courses.id = course_teacher.course_id
+ where teachers.name like 'Fulvio' and teachers.surname like 'Amato';
