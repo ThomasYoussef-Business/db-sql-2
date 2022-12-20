07313c1c186cd11e92403d305b052d26f4218f1b
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 17:43:01 +0100

    Fatta quinta query, collegamento tra lauree, corsi e insegnanti

--- JOIN_5.sql
+++ JOIN_5.sql
@@ -1,3 +1,16 @@
 ﻿/*
- * 
+ * Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
  */
+
+select teachers.name       as 'Teacher''s Name',
+       teachers.surname    as 'Teacher''s Surname',
+       degrees.name        as 'Degree',
+       degrees.level       as 'Degree Level',
+       courses.name        as 'Course',
+       courses.description as 'Course Description',
+       courses.cfu         as 'Course CFU'
+from dbo.teachers
+join dbo.course_teacher on course_teacher.teacher_id = teachers.id
+join dbo.courses on courses.id = course_teacher.course_id
+join dbo.degrees on degrees.id = courses.degree_id
+order by teachers.name, teachers.surname, Degree
