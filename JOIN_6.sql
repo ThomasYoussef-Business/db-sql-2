99e627aec9dd443a218bed2da6881d9828fa63e1
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 17:53:56 +0100

    Fatta sesta query, la query mostra tutti i docenti che insegnano nel departimento di Matematica

--- JOIN_6.sql
+++ JOIN_6.sql
@@ -1,3 +1,17 @@
 ﻿/*
- * 
+ * Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
  */
+
+select distinct teachers.name as 'Teacher''s Name',
+                teachers.surname as 'Teacher''s Surname',
+                teachers.phone as 'Teacher''s Phone number',
+                teachers.email as 'Teacher''s Email',
+                teachers.office_address as 'Teacher''s Office Address',
+                teachers.office_number as 'Teacher''s Office Number'
+from teachers
+join course_teacher on course_teacher.teacher_id = teachers.id
+join courses on courses.id = course_teacher.course_id
+join degrees on degrees.id = courses.degree_id
+join departments on departments.id = degrees.department_id
+where departments.name like '%Matematica'
+order by teachers.name, teachers.surname;
