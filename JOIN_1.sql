d46984f6e8a541be7973816f4a5f7b83282d2512
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 16:31:53 +0100

    Finita prima query

--- JOIN_1.sql
+++ JOIN_1.sql
@@ -1,3 +1,9 @@
 ﻿/*
- * 
+ * Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
  */
+
+select students.id, students.name, students.surname, students.email
+from dbo.students
+join dbo.[degrees] on students.degree_id = [degrees].id
+where [degrees].[name] like '%Laurea in Economia%'
+order by students.name, students.id;
