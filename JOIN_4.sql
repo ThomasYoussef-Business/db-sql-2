cbe68c3604c451e93ed8c4d0895e1d48d923fe60
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-12-20 17:31:19 +0100

    Finita la quarta query, studenti e corsi di laurea collegati con i departimenti

--- JOIN_4.sql
+++ JOIN_4.sql
@@ -1,3 +1,14 @@
 ﻿/*
- * 
+ * Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono
+ * iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
  */
+
+select students.surname as 'Student Surname',
+       students.name	as 'Student Name',
+       degrees.name	    as 'Degree Name',
+       degrees.level	as 'Degree Level',
+       departments.name as 'Department Name'
+from dbo.students
+join dbo.degrees on degrees.id = students.degree_id
+join dbo.departments on departments.id = degrees.department_id
+order by students.surname, students.name;
